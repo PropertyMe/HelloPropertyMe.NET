bd25d58bae5f116d2e006e21983738d38502c6e1
Kun Dai <kundai@propertyme.com>
2019-02-05 10:21:11 +1100

    [PME-4579] Update the scropes n homecontroller

--- src/PM.App.Host/Controllers/HomeController.cs
+++ src/PM.App.Host/Controllers/HomeController.cs
@@ -45,8 +45,8 @@ namespace PM.Api.Host.Controllers
             httpContext.Session.SetObject(accessTokenKey, request);
 
             /// Redirects to the IDP server for authentication. 
-            var endPoint = $"{settings.Value.AuthorityUrl}/connect/authorize?response_type=code&state=&client_id={request.ClientId}&scope=contact:read&redirect_uri={settings.Value.RedirectUrl}";
-
+            //var endPoint = $"{settings.Value.AuthorityUrl}/connect/authorize?response_type=code&state=&client_id={request.ClientId}&scope=contact:read&redirect_uri={settings.Value.RedirectUrl}";
+            var endPoint = $"{settings.Value.AuthorityUrl}/connect/authorize?response_type=code&state=&client_id={request.ClientId}&scope=contact:read activity:read property:read communication:read transaction:read&redirect_uri={settings.Value.RedirectUrl}";
             return Redirect(endPoint);
         }
 
