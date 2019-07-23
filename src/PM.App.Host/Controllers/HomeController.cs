edff68b3030f18cad3fc3ca3ff0928fb04cf75c1
Kun Dai <kundai@propertyme.com>
2019-07-23 15:25:03 +1000

    [PME-4941] before update clientscope data, only allow read scopes

--- src/PM.App.Host/Controllers/HomeController.cs
+++ src/PM.App.Host/Controllers/HomeController.cs
@@ -45,8 +45,8 @@ namespace PM.Api.Host.Controllers
             httpContext.Session.SetObject(accessTokenKey, request);
 
             /// Redirects to the IDP server for authentication. 
-            // var endPoint = $"{settings.Value.AuthorityUrl}/connect/authorize?response_type=code&state=&client_id={request.ClientId}&scope=contact:read%20activity:read%20property:read%20communication:read%20transaction:read&redirect_uri={settings.Value.RedirectUrl}";
-            var endPoint = $"{settings.Value.AuthorityUrl}/connect/authorize?response_type=code&state=&client_id={request.ClientId}&scope=contact:write%20contact:read%20activity:read%20activity:write%20transaction:read%20transaction:write%20property:write%20property:read%20communication:read&redirect_uri={settings.Value.RedirectUrl}";
+            var endPoint = $"{settings.Value.AuthorityUrl}/connect/authorize?response_type=code&state=&client_id={request.ClientId}&scope=contact:read%20activity:read%20property:read%20communication:read%20transaction:read&redirect_uri={settings.Value.RedirectUrl}";
+            //var endPoint = $"{settings.Value.AuthorityUrl}/connect/authorize?response_type=code&state=&client_id={request.ClientId}&scope=contact:write%20contact:read%20activity:read%20activity:write%20transaction:read%20transaction:write%20property:write%20property:read%20communication:read&redirect_uri={settings.Value.RedirectUrl}";
             // var endPoint = $"{settings.Value.AuthorityUrl}/connect/authorize?response_type=code&state=&client_id={request.ClientId}&scope=contact:read&redirect_uri={settings.Value.RedirectUrl}";
             return Redirect(endPoint);
         }
